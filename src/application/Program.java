ae2695457fac430f0655ce35ec42ba2dcea797ed
Diogo Araujo <diogo.arauj@gmail.com>
2020-09-09 22:39:44 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
+import java.util.Scanner;
 import chess.ChessException;
+import java.util.InputMismatchException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Program {
 
